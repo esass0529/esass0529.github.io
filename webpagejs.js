c1464945ca0d231a1ffd3dc41131550abf94fdf8
TaylorChapman <chapmant@bxscience.edu>
2019-07-31 16:27:47 -0400

    initial

---
+++ webpagejs.js
@@ -0,0 +1,11 @@
+function StartingAlert() {
+  alert("CONGRATULATIONS! You have won access to this webpage. This is a huge honor that you are so lucky to receive. Not used to such safe alerts? Well, it is time we changed just that.  ");
+}
+
+
+var stickfigure = "stickfigure.png"
+var showemma = "showemma.jpg"
+
+function ShowEmma() {
+  document.getElementById("ShowEmma").innerHTML = "oops. I guess you'll have to visit my website to find out more!"
+}
