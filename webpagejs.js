94fd6d4183b70f8accfac97583ca03bb651128bf
TaylorChapman <chapmant@bxscience.edu>
2019-08-02 14:41:42 -0400

    Auguest 2

--- webpagejs.js
+++ webpagejs.js
@@ -1,11 +1,35 @@
+
 function StartingAlert() {
   alert("CONGRATULATIONS! You have won access to this webpage. This is a huge honor that you are so lucky to receive. Not used to such safe alerts? Well, it is time we changed just that.  ");
 }
 
 
-var stickfigure = "stickfigure.png"
-var showemma = "showemma.jpg"
 
-function ShowEmma() {
-  document.getElementById("ShowEmma").innerHTML = "oops. I guess you'll have to visit my website to find out more!"
+//slideshow stuff below...
+
+var slideIndex = 0;
+showSlides(slideIndex);
+
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+
+function currentSlide() {
+  showSlides(showIndex = n);
+}
+
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");// Makes a list of images
+
+  if (n > slides.length) {slideIndex = 0}
+  if (n < 0) {slideIndex = slides.length - 1}
+
+  for (i=0; i<slides.length; i++) {//i++ increases i by 1
+    slides[i].style.display = "none";//this will hide the slides first
+  }
+  slides[slideIndex].style.display = "block";
 }
